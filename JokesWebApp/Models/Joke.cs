11ff0328c7750db46affc04aa47c60fa19016090
pauloreis <pmpreis@gmail.com>
2023-10-28 14:26:10 +0100

    Adicionar arquivos de projeto.

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokePergunta { get; set; }
+        public string JokeResposta{ get; set; }
+        public Joke()
+        {
+            Id =0;
+            JokePergunta = string.Empty;
+            JokeResposta = string.Empty;
+        }
+    }
+}
